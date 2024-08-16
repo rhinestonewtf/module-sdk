e6c0fa4d3ac464795efbed2b40dd675f5655c745
yasseinbilal <yassein.bilal@gmail.com>
2024-08-16 16:04:58 +0200

    fix failing test

--- test/unit/module/registy/registry.test.ts
+++ test/unit/module/registy/registry.test.ts
@@ -1,10 +1,11 @@
 import { fetchRegistryModules } from 'src/module'
 import { getPublicClient } from 'test/utils/userOps/clients'
+import { sepolia } from 'viem/chains'
 
 describe('Registry Module', () => {
   it('should get registered modules in the registry', async () => {
     const registeredModules = await fetchRegistryModules({
-      client: getPublicClient(),
+      client: getPublicClient(sepolia),
     })
     expect(registeredModules.length).toBeTruthy()
   })
