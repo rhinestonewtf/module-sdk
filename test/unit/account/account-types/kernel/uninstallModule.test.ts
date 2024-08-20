237fae5770f8aacac1227f2106492e93d796112e
yasseinbilal <yassein.bilal@gmail.com>
2024-08-20 15:10:25 +0200

    fix test

--- test/unit/account/account-types/kernel/uninstallModule.test.ts
+++ test/unit/account/account-types/kernel/uninstallModule.test.ts
@@ -13,7 +13,10 @@ describe('Get calldata to uninstall a module', () => {
   // Setup
   const client = getClient(MockClient)
   const account = getAccount(MockKernelAccountDeployed)
-  const validator = getModule(MockValidator)
+  const validator = getModule({
+    ...MockValidator,
+    module: '0x503b54ed1e62365f0c9e4caf1479623b08acbe77',
+  })
   const executor = getModule(MockExecutor)
   const hook = getModule(MockHook)
   const fallback = getModule(MockFallback)
@@ -24,7 +27,7 @@ describe('Get calldata to uninstall a module', () => {
       module: validator,
     })
 
-    expect(executions.length).toEqual(1)
+    expect(executions.length).toEqual(0)
     // expect(executions[0].target).toEqual(account.address)
     // expect(Number(executions[0].value)).toEqual(0)
     // Todo: decode callData
